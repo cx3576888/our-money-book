e7c406992aeb3921abc1d91be4869251dca64f1c
cx3576888 <cx3576888@gmail.com>
2026-02-20 22:36:17 +0800

    integrate with my google sheet, appendRow to correct tab

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,42 @@ import "./App.css";
 
 import PWABadge from "./PWABadge.tsx";
 
+const GAS_URL =
+  "https://script.google.com/macros/s/AKfycbxTLO5tvIWT9J4FJrVbfX7HDv-8YPSiZFTiNZYJZBx64ZUNklETFNcM_Rn37hPmYdIa/exec";
+
+function getTabName(inputDate: string) {
+  if (inputDate) {
+    return inputDate.substring(0, 7);
+  }
+  const currentTime = new Date();
+  const monthString = String(currentTime.getMonth() + 1);
+  return `${currentTime.getFullYear()}-${monthString.padStart(2, "0")}`;
+}
+
 export default function App() {
   async function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
+    const inputDate = (formData.get("date") ?? "") as string;
     const data = {
-      date: formData.get("date"),
+      tabName: getTabName(inputDate),
+      date: inputDate.substring(5),
       title: formData.get("title"),
       dollar: formData.get("dollar"),
       details: formData.get("details"),
     };
     console.log("hihihi POST data:", data);
+    try {
+      const result = await fetch(GAS_URL, {
+        method: "POST",
+        mode: "no-cors",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      console.log("Success!", result);
+    } catch (error) {
+      console.error("Failed!", error);
+    }
   }
 
   return (
